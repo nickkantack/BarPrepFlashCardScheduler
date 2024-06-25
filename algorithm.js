e9c72817d296ce07d8b91ce190e9ca916bd8e817
nickkantack <nickkantack@gmail.com>
2024-06-24 22:10:32 -0400

    Added basic UI and worked out some logic for adding new cards

---
+++ algorithm.js
@@ -0,0 +1,133 @@
+
+import { Card } from "./card.js";
+import { shuffle, numberOfDaysBetween, daysAfter } from "./utils.js";
+
+const GROUP_POPULATIONS = {
+    "Torts": 44,
+    "Evidence": 42,
+    "Contracts": 42,
+    "ConstitutionalLaw": 52,
+    "CivilProcedure": 64,
+    "CriminalLawAndProcedure": 70,
+    "Property": 65
+}
+
+// Calculate the total number of cards across all groups
+let TOTAL_NUMBER_OF_CARDS = 0;
+[...Object.values(GROUP_POPULATIONS)].forEach(value => {
+    TOTAL_NUMBER_OF_CARDS += value;
+})
+
+// Initialize the map of cards
+const idToCardMap = {};
+[...Object.keys(GROUP_POPULATIONS)].forEach(group => {
+    const cardCount = GROUP_POPULATIONS[group];
+    for (let i = 1; i <= cardCount; i++) {
+        console.log(`The group is ${group}`);
+        const card = new Card(group, i);
+        idToCardMap[card.id] = card;
+    }
+});
+
+// The first element in each card's days array is the start day for reviewing
+// the card. We will exert loose control over this with the assumption that 
+// the user likely wants to assert high control over this. Therefore, we will
+// only automatically generate the study schedule for cards that already have
+// a first day.
+
+// For now, I'm going to start all of the cards on random days, but with the 
+// constraint that they must be as evenly distributed as possible.
+let cardIdsInRandomOrder = [...Object.keys(idToCardMap)];
+shuffle(cardIdsInRandomOrder);
+
+const studyPlanStartDay = Date.parse("23 Jun 2024");
+const lastDayForNewCards = Date.parse("26 Jul 2024");
+const daysInStudyPlan = numberOfDaysBetween(studyPlanStartDay, lastDayForNewCards);
+const newCardsPerDay = Math.ceil(TOTAL_NUMBER_OF_CARDS / daysInStudyPlan);
+console.log(`There are ${daysInStudyPlan} days to learn new cards.`);
+
+let cardsToStudyByDay = [];
+let newCardBuckets = [];
+let newCardBucketSampleCounters = [];
+
+generateStudyPlan();
+
+function generateStudyPlan() {
+
+    cardsToStudyByDay = [];
+
+    for (let i = 0; i < daysInStudyPlan; i++) cardsToStudyByDay.push([]);
+
+    // TODO Don't randomly create the new card buckets. Manually make them based on
+    // user input. Also, allow future new card buckets to be empty. This means this
+    // tool only prescribes how to study old cards but does not dictate which new
+    // cards to add to the study rotation (user gets to do that later).
+    for (let i = 0; i < daysInStudyPlan; i++) {
+        newCardBuckets.push([]);
+        newCardBucketSampleCounters.push(0);
+    }
+
+    // ----------------------------------------------------------------------------
+    // Manually configure new card buckets here
+    // ----------------------------------------------------------------------------
+    // TODO parse the HTML and fill the new card buckets
+    newCardBuckets[0] = [...Object.values(idToCardMap)].filter(x => x.group === "Torts").slice(0, 12);
+
+    console.log(newCardBuckets);
+        
+    /*
+    for (let i = 0; i < TOTAL_NUMBER_OF_CARDS; i++) {
+        const bucketIndex = Math.floor(i / newCardsPerDay);
+        newCardBuckets[bucketIndex].push(idToCardMap[cardIdsInRandomOrder[i]]);
+        newCardBucketSampleCounters.push(0);
+    }
+    */
+
+    // ----------------------------------------------------------------------------
+    // Generate study plan
+    // ----------------------------------------------------------------------------
+    for (let i = 0; i < daysInStudyPlan; i++) {
+
+        // Give it all of the cards from the corresponding new card bucket and the
+        // previous new card bucket, if one exists
+        newCardBuckets[i].forEach(card => {
+            cardsToStudyByDay[i].push(card);
+        });
+
+        // Sample cards from previous new card buckets
+        for (let stepsBack = 1; stepsBack <= i; stepsBack++) {
+            const sampleCounterIncrement = newCardsPerDay * Math.pow(.6, stepsBack - 1);
+            const oldBucketIndex = i - stepsBack;
+            newCardBucketSampleCounters[oldBucketIndex] += sampleCounterIncrement;
+            const cardsToSample = Math.floor(newCardBucketSampleCounters[oldBucketIndex]);
+            newCardBucketSampleCounters[oldBucketIndex] -= cardsToSample;
+            for (let k = 0; k < cardsToSample; k++) {
+                // Find the card in the old card bucket that is tied for being the
+                // least practiced and add it to the bucket
+                let fewestTimesPracticed = null;
+                let leastPracticedCard = null;
+                newCardBuckets[oldBucketIndex].forEach(card => {
+                    const timesThisCardWasPracticed = card.days.length;
+                    if (fewestTimesPracticed == null || (timesThisCardWasPracticed < fewestTimesPracticed)) {
+                        leastPracticedCard = card;
+                        fewestTimesPracticed = timesThisCardWasPracticed;
+                    }
+                });
+
+                // The old card bucket is allowed to be empty for some days and this
+                // manifests here as a null leastPracticedCard. Just don't sample the
+                // bucket in this case.
+                if (leastPracticedCard) {
+                    leastPracticedCard.days.push(daysAfter(studyPlanStartDay, i));
+                    cardsToStudyByDay[i].push(leastPracticedCard);
+                }
+            }
+        }
+    }
+
+    console.log(cardsToStudyByDay);
+
+    // TODO update the HTML old card tables
+}
+
+export { GROUP_POPULATIONS }
--- main.js
+++ main.js
@@ -1,123 +1,68 @@
+import { GROUP_POPULATIONS } from "./algorithm.js";
 
-import { Card } from "./card.js";
-import { shuffle, numberOfDaysBetween, daysAfter } from "./utils.js";
+const addNewCardButton = document.getElementById(`addNewCardButton`);
+const cardDivTemplate = document.getElementById(`cardTemplate`);
 
-const GROUP_POPULATIONS = {
-    "Torts": 44,
-    "Evidence": 42,
-    "Constracts": 42,
-    "ConstitutionalLaw": 52,
-    "CivilProcedure": 64,
-    "CriminalLawAndProcedure": 70,
-    "Property": 65
-}
-
-// Calculate the total number of cards across all groups
-let TOTAL_NUMBER_OF_CARDS = 0;
-[...Object.values(GROUP_POPULATIONS)].forEach(value => {
-    TOTAL_NUMBER_OF_CARDS += value;
-})
-
-// Initialize the map of cards
-const idToCardMap = {};
-[...Object.keys(GROUP_POPULATIONS)].forEach(group => {
-    const cardCount = GROUP_POPULATIONS[group];
-    for (let i = 1; i <= cardCount; i++) {
-        console.log(`The group is ${group}`);
-        const card = new Card(group, i);
-        idToCardMap[card.id] = card;
-    }
-});
-
-// The first element in each card's days array is the start day for reviewing
-// the card. We will exert loose control over this with the assumption that 
-// the user likely wants to assert high control over this. Therefore, we will
-// only automatically generate the study schedule for cards that already have
-// a first day.
-
-// For now, I'm going to start all of the cards on random days, but with the 
-// constraint that they must be as evenly distributed as possible.
-let cardIdsInRandomOrder = [...Object.keys(idToCardMap)];
-shuffle(cardIdsInRandomOrder);
-
-const studyPlanStartDay = Date.parse("23 Jun 2024");
-const lastDayForNewCards = Date.parse("26 Jul 2024");
-const daysInStudyPlan = numberOfDaysBetween(studyPlanStartDay, lastDayForNewCards);
-const newCardsPerDay = Math.ceil(TOTAL_NUMBER_OF_CARDS / daysInStudyPlan);
-console.log(`There are ${daysInStudyPlan} days to learn new cards.`);
-
-let cardsToStudyByDay = [];
-for (let i = 0; i < daysInStudyPlan; i++) cardsToStudyByDay.push([]);
-
-// Create a "new card" bucket for each day. These are equally filled and their
-// contents do not change. Each bucket will have a sampling counter that grows
-// each day but at a diminishing rate in time.
-let newCardBuckets = [];
-let newCardBucketSampleCounters = [];
-// TODO Don't randomly create the new card buckets. Manually make them based on
-// user input. Also, allow future new card buckets to be empty. This means this
-// tool only prescribes how to study old cards but does not dictate which new
-// cards to add to the study rotation (user gets to do that later).
-for (let i = 0; i < daysInStudyPlan; i++) {
-    newCardBuckets.push([]);
-    newCardBucketSampleCounters.push(0);
-}
-
-// ----------------------------------------------------------------------------
-// Manually configure new card buckets here
-// ----------------------------------------------------------------------------
-newCardBuckets[0] = [...Object.values(idToCardMap)].filter(x => x.group === "Torts").slice(0, 12);
-
-console.log(newCardBuckets);
-    
-/*
-for (let i = 0; i < TOTAL_NUMBER_OF_CARDS; i++) {
-    const bucketIndex = Math.floor(i / newCardsPerDay);
-    newCardBuckets[bucketIndex].push(idToCardMap[cardIdsInRandomOrder[i]]);
-    newCardBucketSampleCounters.push(0);
-}
-*/
-
-// ----------------------------------------------------------------------------
-// Generate study plan
-// ----------------------------------------------------------------------------
-for (let i = 0; i < daysInStudyPlan; i++) {
-
-    // Give it all of the cards from the corresponding new card bucket and the
-    // previous new card bucket, if one exists
-    newCardBuckets[i].forEach(card => {
-        cardsToStudyByDay[i].push(card);
+addNewCardButton.addEventListener(`click`, () => {
+    const cardDiv = cardDivTemplate.content.cloneNode(true).querySelector(`div`);
+    const groupSelect = cardDiv.querySelector(`select.groupSelect`);
+    [...Object.keys(GROUP_POPULATIONS)].forEach(group => {
+        const option = document.createElement(`option`);
+        option.value = group;
+        option.innerHTML = group;
+        groupSelect.appendChild(option);
+    });
+    const indexSelect = groupSelect.closest(`table`).querySelector(`select.indexSelect`);
+    groupSelect.addEventListener(`change`, (e) => {
+        const selectedGroup = e.target.value;
+        const selectedIndex = indexSelect.selectedIndex;
+        indexSelect.innerHTML = ``;
+        for (let i = 1; i <= GROUP_POPULATIONS[selectedGroup]; i++) {
+            const option = document.createElement(`option`);
+            option.value = i;
+            option.innerHTML = i;
+            indexSelect.appendChild(option);
+        }
+        if (selectedIndex >= 0 && selectedIndex < GROUP_POPULATIONS[selectedGroup]) {
+            indexSelect.selectedIndex = selectedIndex;
+        } else {
+            indexSelect.selectedIndex = -1;
+        }
+        // TODO maybe trigger an update of the study plan if the index is also
+        // populated
+        removeNewCardDuplicates(cardDiv);
+    });
+    indexSelect.addEventListener(`change`, (e) => {
+        // TODO maybe trigger an update of the study plan if the group is also
+        // populated
+        removeNewCardDuplicates(cardDiv);
     });
 
-    // Sample cards from previous new card buckets
-    for (let stepsBack = 1; stepsBack <= i; stepsBack++) {
-        const sampleCounterIncrement = newCardsPerDay * Math.pow(.6, stepsBack - 1);
-        const oldBucketIndex = i - stepsBack;
-        newCardBucketSampleCounters[oldBucketIndex] += sampleCounterIncrement;
-        const cardsToSample = Math.floor(newCardBucketSampleCounters[oldBucketIndex]);
-        newCardBucketSampleCounters[oldBucketIndex] -= cardsToSample;
-        for (let k = 0; k < cardsToSample; k++) {
-            // Find the card in the old card bucket that is tied for being the
-            // least practiced and add it to the bucket
-            let fewestTimesPracticed = null;
-            let leastPracticedCard = null;
-            newCardBuckets[oldBucketIndex].forEach(card => {
-                const timesThisCardWasPracticed = card.days.length;
-                if (fewestTimesPracticed == null || (timesThisCardWasPracticed < fewestTimesPracticed)) {
-                    leastPracticedCard = card;
-                    fewestTimesPracticed = timesThisCardWasPracticed;
-                }
-            });
+    // Dispatch a change event to initialize the index select
+    // const starterEvent = new Event(`change`);
+    // groupSelect.dispatchEvent(starterEvent);
+    groupSelect.selectedIndex = -1;
+    indexSelect.selectedIndex = -1;
 
-            // The old card bucket is allowed to be empty for some days and this
-            // manifests here as a null leastPracticedCard. Just don't sample the
-            // bucket in this case.
-            if (leastPracticedCard) {
-                leastPracticedCard.days.push(daysAfter(studyPlanStartDay, i));
-                cardsToStudyByDay[i].push(leastPracticedCard);
-            }
+    const newCardsForDayDiv = document.querySelector(`div.dayPanel`).querySelector(`.newCardsForDayDiv`);
+    newCardsForDayDiv.appendChild(cardDiv);
+});
+
+function removeNewCardDuplicates(cardDiv) {
+    const newCardsForDayDiv = document.querySelector(`div.newCardsForDayDiv`);
+    const groupSelect = cardDiv.querySelector(`select.groupSelect`);
+    const indexSelect = cardDiv.querySelector(`select.indexSelect`);
+    [...newCardsForDayDiv.querySelectorAll(`.cardDiv`)].forEach(otherCardDiv => {
+        if (otherCardDiv === cardDiv) return;
+        const otherGroupSelect = otherCardDiv.querySelector(`select.groupSelect`);
+        const otherIndexSelect = otherCardDiv.querySelector(`select.indexSelect`);
+        const otherGroup = otherGroupSelect.options[otherGroupSelect.selectedIndex].value;
+        const otherIndex = otherIndexSelect.options[otherIndexSelect.selectedIndex].value;
+        if (groupSelect.selectedIndex < 0 || indexSelect.selectedIndex < 0) return;
+        const thisGroup = groupSelect.options[groupSelect.selectedIndex].value;
+        const thisIndex = indexSelect.options[indexSelect.selectedIndex].value;
+        if (otherGroup === thisGroup && otherIndex === thisIndex) {
+            otherCardDiv.remove();
         }
-    }
+    });
 }
-
-console.log(cardsToStudyByDay);
